8561c58984b85be7f5c790c401e170408a4417ef
Julian Salinas <julian4001@outlook.com>
2017-10-09 01:08:32 -0600

    Arreglo de bug en el conversor png

--- Codigo/modelo/utilitarios/conversor.py
+++ Codigo/modelo/utilitarios/conversor.py
@@ -13,10 +13,12 @@ class Conversor:
 
         img_pgm = cv.imread(ruta_img_pgm, 0)
         ruta, nombre = os.path.split(ruta_img_pgm)
-        nombre = os.path.splitext(ruta)[0] + ".png"
+        nombre = os.path.splitext(nombre)[0] + ".png"
         ruta_imagen_png = os.path.join(ruta, nombre)
         cv.imwrite(ruta_imagen_png, img_pgm)
 
         return ruta_imagen_png
 
 # ----------------------------------------------------------------------------------------------------------------------
+
+print(Conversor.convertir_pgm_a_png("estapa\\medio\\paup.pgm"))
