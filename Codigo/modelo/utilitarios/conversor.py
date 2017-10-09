29bafb3c3a315948924fc6e1da016801834a3f9f
Armando López Cordero <arlopez@ic-itcr.ac.cr>
2017-10-09 03:03:12 -0600

    Despliegue de la imagen correctamente

--- Codigo/modelo/utilitarios/conversor.py
+++ Codigo/modelo/utilitarios/conversor.py
@@ -9,13 +9,20 @@ import cv2 as cv
 class Conversor:
 
     @staticmethod
-    def convertir_pgm_a_png(ruta_img_pgm):
+    def guardar_imagen(ruta_img_pgm):
 
         img_pgm = cv.imread(ruta_img_pgm, 0)
         ruta, nombre = os.path.split(ruta_img_pgm)
         nombre = os.path.splitext(nombre)[0] + ".png"
         ruta_imagen_png = os.path.join(ruta, nombre)
         cv.imwrite(ruta_imagen_png, img_pgm)
+
+    @staticmethod
+    def convertir_pgm_a_png(ruta_img_pgm):
+
+        ruta, nombre = os.path.split(ruta_img_pgm)
+        nombre = os.path.splitext(nombre)[0] + ".png"
+        ruta_imagen_png = os.path.join(ruta, nombre)
 
         return ruta_imagen_png
 
